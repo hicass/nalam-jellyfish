406f59dbae5295ce379e2a7774914b6304cb0ba8
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-11 23:48:54 -0800

    resolved merge conflicts with DonorLandingPage

--- src/pages/DonorLandingPage.jsx
+++ src/pages/DonorLandingPage.jsx
@@ -1,13 +1,15 @@
 import DonorProfile from "../components/DonorProfile";
 import MoreSupport from "../components/MoreSupport";
+import logo from "../assets/nalamlogo.png";
 
 export default function DonorLandingPage() {
   return (
-    <div className="flex justify-center bg-[#EFF1FF]">
-      <DonorProfile />
-      <div className="w-3/5">
+    <section className="bg-[#EFF1FF] px-16 py-6">
+      <img src={logo} alt="nalam" className="mb-12 ml-16 w-20 pb-12 pt-6" />
+      <div className="flex flex-row">
+        <DonorProfile />
         <MoreSupport />
       </div>
-    </div>
+    </section>
   );
 }
