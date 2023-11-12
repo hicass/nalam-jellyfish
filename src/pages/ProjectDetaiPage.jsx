74942e07d8fd01fe9c00770cb2a0a8f93b78e549
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-12 09:59:09 -0800

    added header to project page

--- src/pages/ProjectDetaiPage.jsx
+++ src/pages/ProjectDetaiPage.jsx
@@ -1,17 +1,21 @@
 import ProjectPageLeft from "../components/ProjectPageLeft";
 import ProjectPageGoal from "../components/ProjectPageGoal";
 import SocialMediaShare from "../components/SocialMediaShare";
+import Header from "../components/Header";
 
 export default function ProjectDetailPage() {
   return (
-    <section className="flex">
-      <div className="w-2/6">
-        <ProjectPageLeft />
-      </div>
-      <div className="flex flex-grow flex-col">
-        <ProjectPageGoal />
-        <SocialMediaShare />
-      </div>
-    </section>
+    <div>
+      <Header />
+      <section className="flex">
+        <div className="w-2/6">
+          <ProjectPageLeft />
+        </div>
+        <div className="flex flex-grow flex-col">
+          <ProjectPageGoal />
+          <SocialMediaShare />
+        </div>
+      </section>
+    </div>
   );
 }
