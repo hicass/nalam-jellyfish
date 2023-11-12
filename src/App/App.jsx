68275a5bdc6d873625473a6936edf85f948b26d5
hicass <cassidyjwalters@gmail.com>
2023-11-11 21:50:25 -0800

    Set up skeleton and routes.

---
+++ src/App/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import DonorLandingPage from '../pages/DonorLandingPage';
+import ProjectDetailPage from '../pages/ProjectDetaiPage';
+import './App.css';
+
+function App() {
+  return (
+    <main className="App">
+      <Routes>
+        <Route path="/donor" element={<DonorLandingPage />} />
+        <Route path="/donor" element={<ProjectDetailPage />} />
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
